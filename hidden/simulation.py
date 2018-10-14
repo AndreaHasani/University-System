c7e35c14016d3711cacb3d37fb184d3de928af4a
Andrea Hasani <pasqyra3d@gmail.com>
2018-10-14 15:55:29 +0200

    Changes

--- hidden/simulation.py
+++ hidden/simulation.py
@@ -2,6 +2,8 @@ import requests
 from faker import Faker
 import random
 import os
+import sqlite3 as sql
+
 os.environ['no_proxy'] = '127.0.0.1,localhost'
 
 
@@ -13,20 +15,57 @@ deget = ["Maths", "English", "History",
          "Astronomy", "Computer", "Science",
          "Algebra", "Geomtry", "Calculus"]
 
-for user in range(200):
+db = sql.connect("./database/universitySystem.db")
+c = db.cursor()
+
+createTable = '''
+CREATE TABLE IF NOT EXISTS Shkolla (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+password TEXT NOT NULL UNIQUE,
+birthday TEXT NOT NULL,
+subjects TEXT,
+subjects_failed TEXT,
+avarage INTEGER NOT NULL,
+permission TEXT NOT NULL
+);'''
+
+c.execute(createTable)
+
+InsertRecord = """
+INSERT OR IGNORE INTO Shkolla (
+id,
+name,
+password,
+birthday,
+subjects,
+subjects_failed,
+avarage,
+permission)
+VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)
+"""
+
+# Admin User | User: Demo, Password: Demo
+c.execute(InsertRecord, ('demo', 'demo', '2014-01-22',
+                         None, None, 0, 'admin'))
+
+db.commit()
+
+addRecords = []
+for user in range(50000):  # Simulation 50000 Users
     name = fake.name()
     password = fake.password(length=8, special_chars=False)
     birthday = fake.date_this_century()
-    subjects = random.sample(deget, 12)
     subjects_failed = random.sample(deget, random.randint(0, 4))
+    subjects = [subject for subject in random.sample(
+        deget, 12) if subject not in subjects_failed]
     avarage = random.uniform(6, 10)
 
-    args = {'name': name, 'password': password,
-            'birthday': str(birthday),
-            'subjects': ', '.join(
-                subjects), 'subjectsFailed': ', '.join(subjects_failed),
-            'avarage': int( float( '%.2f' % avarage ) * 100 ), 'permission': 'user'}
-    print(args)
+    addRecords.append([name, password, str(birthday), ', '.join(subjects),
+                       ', '.join(subjects_failed),
+                       int(float('%.2f' % avarage) * 100), "user"])
 
-    with requests.post("http://127.0.0.1:5005/rest/adduser", data=args) as resp:
-        print(resp.text)
+c.executemany(InsertRecord, addRecords)
+
+db.commit()
+db.close()
